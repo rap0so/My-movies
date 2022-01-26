d776a1a7e59799779629ffb3b347af98d646e4c0
Rap0so <william.s.godoy53@gmail.com>
2022-01-26 20:19:43 -0300

    feat(app): wrap application within queryclient

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { RouteWrapper } from 'routes';
 import theme from 'theme';
 
+const queryClient = new QueryClient();
+
 const App = () => (
   <ChakraProvider theme={theme}>
-    <RouteWrapper />
+    <QueryClientProvider client={queryClient}>
+      <RouteWrapper />
+    </QueryClientProvider>
   </ChakraProvider>
 );
 
