a20b0f24725893170aee4b6e23ff850af56397a8
Rap0so <william.s.godoy53@gmail.com>
2022-01-26 12:47:34 -0300

    feat: create Logo component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,7 @@
+import { Image } from '@chakra-ui/react';
+
+import logoImage from './logo.png';
+
+const Logo = () => <Image maxW="100px" src={logoImage} />;
+
+export default Logo;
---
+++ src/components/Logo/types.ts
@@ -0,0 +1,1 @@
+export type TLogoProps = any;
