6917ffb7b999cb9e7f9ad224a0a1495aef96ed37
Rap0so <william.s.godoy53@gmail.com>
2022-01-27 15:59:44 -0300

    style(card): organize within swiper

--- src/components/Card/card.style.ts
+++ src/components/Card/card.style.ts
@@ -11,14 +11,18 @@ export const cardStyle: CSSObject = {
   maxH: '130px',
   overflow: 'hidden',
   transition: 'all .5s ease',
-  w: '200px',
+  w: '100px',
 
   _hover: {
     '.controls': {
       opacity: 1,
     },
+    bgColor: '#000',
+    boxShadow: '0 0 100px 20px #000',
     maxH: '340px',
+    pos: 'relative',
     transform: 'scale(1.5)',
     transition: 'all .5s ease',
+    zIndex: 1,
   },
 };
