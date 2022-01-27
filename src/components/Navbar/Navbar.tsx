19f7375f4cc3dcc60b7439cccbed0444d6eedbf1
Rap0so <william.s.godoy53@gmail.com>
2022-01-27 19:30:45 -0300

    style(navbar): increase spaces

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,13 +5,11 @@ import { TNavbarProps } from './types';
 import { Flex } from '@chakra-ui/react';
 import SearchBar from 'components/SearchBar';
 
-const Navbar: FC<TNavbarProps> = ({ onQuery }) => {
-  return (
-    <Flex justifyContent="space-between" px="15px" py="20px">
-      <Logo />
-      <SearchBar onQuery={onQuery} />
-    </Flex>
-  );
-};
+const Navbar: FC<TNavbarProps> = ({ onQuery }) => (
+  <Flex justifyContent="space-between" px="20" py="20px">
+    <Logo />
+    <SearchBar onQuery={onQuery} />
+  </Flex>
+);
 
 export default Navbar;
