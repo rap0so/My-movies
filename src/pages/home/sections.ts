03271989c7239e36fbdd365feb6e610bd134c6b7
Rap0so <william.s.godoy53@gmail.com>
2022-01-27 19:28:04 -0300

    feat(sections): create list

---
+++ src/pages/home/sections.ts
@@ -0,0 +1,18 @@
+import { getMoviesByCategory } from 'api';
+
+const sections = [
+  {
+    method: { action: getMoviesByCategory('upcoming'), name: 'getUpcoming' },
+    title: 'The Best of Upcoming',
+  },
+  {
+    method: { action: getMoviesByCategory('top_rated'), name: 'getTopRated' },
+    title: 'PopCorn & Top Rated',
+  },
+  {
+    method: { action: getMoviesByCategory('popular'), name: 'getPopular' },
+    title: 'Everybody is talking about',
+  },
+];
+
+export default sections;
