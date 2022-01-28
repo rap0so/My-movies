b495526027340f1f7aa3340184a858264e6aed94
Rap0so <william.s.godoy53@gmail.com>
2022-01-28 13:34:27 -0300

    style(swiper): add breakpoints

--- src/sections/SliderMovies/swiperConfigs.ts
+++ src/sections/SliderMovies/swiperConfigs.ts
@@ -1,7 +1,28 @@
-const swiperConfigs = {
+import { SwiperProps } from 'swiper/react';
+
+const swiperConfigs: SwiperProps = {
+  breakpoints: {
+    1600: {
+      slidesPerView: 7,
+    },
+    1366: {
+      slidesPerView: 6,
+    },
+    1200: {
+      slidesPerView: 5.2,
+    },
+    900: {
+      slidesPerView: 4,
+    },
+    700: {
+      slidesPerView: 3,
+    },
+    500: {
+      slidesPerView: 2.2,
+    },
+  },
   navigation: true,
-  slidesPerView: 8.8,
-  spaceBetween: 50,
+  slidesPerView: 1.7,
 };
 
 export default swiperConfigs;
