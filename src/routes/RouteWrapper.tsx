6ce32ff4b8575f13c253679ea90168a3e813d194
Rap0so <william.s.godoy53@gmail.com>
2022-01-28 02:05:15 -0300

    feat(routes): invoke navbar before routes

--- src/routes/RouteWrapper.tsx
+++ src/routes/RouteWrapper.tsx
@@ -4,10 +4,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Loading from 'components/Loading';
 
 import routes from '.';
+import Navbar from 'components/Navbar';
 
 const RouteWrapper = () => (
   <Suspense fallback={<Loading full={true} />}>
     <Router>
+      <Navbar />
+
       <Routes>
         {routes.map(({ path, Component }) => (
           <Route key={path} path={path} element={<Component />} />
