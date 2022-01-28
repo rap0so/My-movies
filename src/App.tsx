cfb24cf086b64d91ef2734d1cf76098f1c1a3e67
Rap0so <william.s.godoy53@gmail.com>
2022-01-28 01:59:56 -0300

    feat(app): add toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import SwiperCore, { Navigation } from 'swiper';
+
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { RouteWrapper } from 'routes';
 import theme from 'theme';
-import SwiperCore, { Navigation } from 'swiper';
+import { ToastContainer } from 'react-toastify';
 
 import 'swiper/css/bundle';
 import 'swiper/css/pagination';
+import 'react-toastify/dist/ReactToastify.css';
 
 SwiperCore.use([Navigation]);
 
 const queryClient = new QueryClient();
 
 const App = () => (
-  <ChakraProvider theme={theme}>
-    <QueryClientProvider client={queryClient}>
-      <RouteWrapper />
-    </QueryClientProvider>
-  </ChakraProvider>
+  <>
+    <ChakraProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <RouteWrapper />
+      </QueryClientProvider>
+    </ChakraProvider>
+    <ToastContainer hideProgressBar={false} pauseOnHover={true} />
+  </>
 );
 
 export default App;
