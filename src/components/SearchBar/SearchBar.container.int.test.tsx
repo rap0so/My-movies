c6b92cd41dac2b4e37324e57f65776dcfb1f0b11
Rap0so <william.s.godoy53@gmail.com>
2022-01-30 02:44:39 -0300

    test(gallery): ensure component

--- src/components/SearchBar/SearchBar.container.int.test.tsx
+++ src/components/SearchBar/SearchBar.container.int.test.tsx
@@ -1,8 +1,5 @@
-import { cleanup, render, waitFor } from '@testing-library/react';
+import { cleanup, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { createRef } from 'react';
-
-import fakeMovies from '__mocks__/movies.json';
 
 import SearchBarContainer from '.';
 
@@ -24,7 +21,7 @@ describe('SearchBarContainer', () => {
     render(<SearchBarContainer onQuery={onQuery} />);
   });
 
-  fit('should invoke onQuery when type on input', async () => {
+  it('should invoke onQuery when type on input', async () => {
     const { getByRole } = render(<SearchBarContainer onQuery={onQuery} />);
 
     const input = getByRole('textbox');
@@ -34,25 +31,5 @@ describe('SearchBarContainer', () => {
     jest.runAllTimers();
 
     expect(onQuery).toHaveBeenLastCalledWith('the amazing movie');
-
-    // expect(onQuery).toHaveBeenLastCalledWith('the amazing movie');
-  });
-
-  xit('should execute close callback when exit', async () => {
-    const { findByRole } = render(
-      <SearchBarContainer
-        movies={fakeMovies}
-        query="spiderman"
-        rootRef={createRef()}
-        isOpen={isOpen}
-        onClose={onClose}
-      />,
-    );
-
-    const closeButton = await findByRole('button', { name: 'Close' });
-
-    userEvent.click(closeButton);
-
-    expect(onClose).toHaveBeenCalled();
   });
 });
