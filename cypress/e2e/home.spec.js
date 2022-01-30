38102bc773a138d62d0ad639e0a1ef9cc706ce83
Rap0so <william.s.godoy53@gmail.com>
2022-01-30 08:17:05 -0300

    test(e2e): search guarded

--- cypress/e2e/home.spec.js
+++ cypress/e2e/home.spec.js
@@ -18,6 +18,44 @@ it('should see the movies', () => {
   cy.get('.swiper-container').find('img').should('have.length', 60);
 });
 
+it('should see the movie description when click on movie image', () => {
+  cy.get('.swiper-container').first().find('img').first().click();
+  cy.get('.chakra-modal__content').should('be.visible');
+  cy.contains('Scream');
+  cy.contains(
+    'Twenty-five years after a streak of brutal murders shocked the quiet town of Woodsboro, a new killer has donned the Ghostface mask and begins targeting a group of teenagers to resurrect secrets from the town’s deadly past.',
+  );
+  cy.get('.chakra-modal__close-btn').click();
+});
+
+it('should search for movies', () => {
+  cy.intercept('GET', '**/search/movie/**', {
+    fixture: 'search',
+  });
+  cy.get('[data-testid="searchbar"]')
+    .find('[data-testid="search-wrapper"]')
+    .click();
+
+  cy.get('[data-testid="searchbar"]').find('input').type('spiderman');
+  cy.get('.chakra-modal__body').as('searchResult');
+  cy.get('@searchResult').should('be.visible');
+
+  cy.get('@searchResult')
+    .find('[data-testid="card-container"]')
+    .as('movies')
+    .should('have.length', 20);
+
+  cy.get('@searchResult').scrollTo('bottom');
+
+  cy.wait(3000);
+
+  cy.get('@searchResult')
+    .find('[data-testid="card-container"]')
+    .should('have.length', 40);
+
+  cy.get('.chakra-modal__close-btn').click();
+});
+
 it('should load more movies when click on next button', () => {
   cy.get('.swiper-container').first().as('firstSection');
 
